275b1616f8332c4ddce36f10290518078b1dbc50
WebDwithAvi <aviral852111@gmail.com>
2025-05-27 14:51:45 +0530

    secondd

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -1,48 +1,52 @@
 import User from "../models/User.js";
-import { Webhook } from "svix";
+import { messageInRaw, Webhook } from "svix";
 
-const clerkWebhooks=async(req,res)=>{
+const clerkWebhooks = async (req, res) => {
     try {
-        const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        const headers={
-            "svix-id":req.headers["svix-id"],
-            "svix-timestamp":req.headers["svix-timestamp"],
-            "svix-signature":req.headers["svix-signature"]
+        // create a svix instance with clerk webhook secret
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+        // getting headers
+        const headers = {
+            "svix-id": req.headers["svid-id"],
+            "svix-timestamp": req.headers["svid-timestamp"],
+            "svix-signature": req.headers["svid-signature"]
         };
-        await whook.verify(JSON.stringify(req.body),headers)
-        const{data,type}=req.body
-        const userData={
-            _id:data.id,
-            email:data.email_addresses[0].email_address,
-            username:data.first_name+" "+data.last_name,
-            image:data.image_url,
+
+        // verifying headers
+        await whook.verify(JSON.stringify(req.body), headers)
+
+        // getting data and event type from request body
+        const { data, type } = req.body;
+
+        const userData = {
+            _id: data.id,
+            email: data.email_addresses[0].email_address,
+            username: data.first_name + " " + data.last_name,
+            image: data.image_url
         }
-        switch(type){
-            case "user.created":{
+
+        // switch case for diff events
+        switch (type) {
+            case "user.created":
                 await User.create(userData);
                 break;
-            }
-            case "user.updated":{
-                await User.findByIdandUpdate(data.id,userData);
+            case "user.updated":
+                await User.findByIdAndUpdate(data.id, userData);
                 break;
-            }
-            case "user.deleted":{
-                await User.findByIdandDelete(data.id);
+            case "user.deleted":
+                await User.findByIdAndDelete(data.id, userData);
                 break;
-            }
-            
+
             default:
-                break;    
+                break;
         }
-        res.json({
-            success:true,
-            message:error.message
-        })
+        res.json({ success: true, message: "Webhook received" })
+
     } catch (error) {
-        res.json({
-            success:false,
-            message:"invalid"
-        })
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
     }
+
 }
-export default clerkWebhooks;
+export default clerkWebhooks
