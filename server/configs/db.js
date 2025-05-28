e8a5303ed05f6d36d588e631b3f8fd4136885765
WebDwithAvi <aviral852111@gmail.com>
2025-05-28 16:46:59 +0530

    new connect db

--- server/configs/db.js
+++ server/configs/db.js
@@ -4,7 +4,13 @@ import mongoose from "mongoose";
         mongoose.connection.on('connected',()=>
             console.log("Database connected")
         )
-        await mongoose.connect(`${process.env.MONGO_URI}/hotel-booking`)
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName: "hotel-booking",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
+            bufferCommands: false
+          });
     } catch (error) {
         console.log(error.message)
     }
